b5fbf04cea4a1ddc59b1d2f4df412f5c00cfef4e
arhamh7 <90596316+arhamh7@users.noreply.github.com>
2023-02-06 16:19:18 -0700

    Update A1.java

--- A1.java
+++ A1.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 /** 
@@ -15,66 +16,30 @@ import java.util.Scanner;
 
 public class A1 {
 
-	public String[][] avengerRoster = { { "captainamerica", "rogers" }, { "ironman", "stark" },
-			{ "blackwidow", "romanoff" }, { "hulk", "banner" }, { "blackpanther", "tchalla" }, { "thor", "odinson" },
-			{ "hawkeye", "barton" }, { "warmachine", "rhodes" }, { "spiderman", "parker" },
-			{ "wintersoldier", "barnes" } };
+    private int topN = 4;
+    private int totalwordcount = 0;
+    private ArrayList<Avenger> avengersArrayList = new ArrayList<>();
+    private String[][] avengerRoster = new String[][]{
+        {"captainamerica", "rogers"}, {"ironman", "stark"},
+        {"blackwidow", "romanoff"}, {"hulk", "banner"}, {"blackpanther", "tchalla"}, {"thor", "odinson"},
+        {"hawkeye", "barton"}, {"warmachine", "rhodes"}, {"spiderman", "parker"},
+        {"wintersoldier", "barnes"}
+    };
 
-	private int topN = 4;
-	private int totalwordcount = 0;
-	private ArrayList<Avenger> avengersArrayList = new ArrayList<>();
+    public static void main(String[] args) throws FileNotFoundException {
+        A1 object = new A1();
+        object.run();
 
-	public static void main(String[] args) throws FileNotFoundException {
-		A1 a1 = new A1();
-		a1.run();
-	}
+    }
 
-	public void run() throws FileNotFoundException {
-		readInput();
-		printResults();
-	}
+    public void run() throws FileNotFoundException {
+        readInput();
+        printResults();
+    }
 
-	/**
-	 * read the input stream and keep track  
-	 * how many times avengers are mentioned by alias or last name.
-	 * @throws FileNotFoundException 
-	 */
-	
-	
-	private void readInput() throws FileNotFoundException {
-		File file = new File("input1.txt");
-		Scanner scan = new Scanner(file); 
+    public void readInput() throws FileNotFoundException {
 
-		while (scan.hasNext()) {
-			String word = scan.next().toLowerCase().replaceAll("[^a-z]", "");
-			if (!word.isEmpty()) {
-				totalwordcount++;
-				boolean found = false;
-				for (String[] avenger : avengerRoster) {
-					if (word.equals(avenger[0]) || word.equals(avenger[1])) {
-						found = true;
-						boolean alreadyAdded = false;
-						for (Avenger a : avengersArrayList) {
-							if (a.toString().equals(avenger[0])) {
-								alreadyAdded = true;
-								a.incrementFrequency();
-								break;
-							}
-						}
-						if (!alreadyAdded) {
-							avengersArrayList.add(new Avenger(avenger[0], avenger[1]));
-							avengersArrayList.get(avengersArrayList.size() - 1).incrementFrequency();
-
-						}
-						break;
-					}
-				}
-			}
-		}
-		scan.close();
-	}
-
-		/*
+        /*
 		In a loop, while the scanner object has not reached end of stream,
 		 	- read a word.
 		 	- clean up the word
@@ -83,38 +48,118 @@ public class A1 {
 				- Create a new avenger object with the corresponding alias and last name.
 				- if this avenger has already been mentioned, increase the frequency count for the object already in the list.
 				- if this avenger has not been mentioned before, add the newly created avenger to the list, remember to set the frequency.
-		*/ 
+         */
+        File file = new File("input1.txt");
+        Scanner scan = new Scanner(file);
 
-	/**
-	 * print the results
-	 */
-	private void printResults() {
-		System.out.println("Total number of words: " + totalwordcount);
-		System.out.println("Number of Avengers Mentioned: " + avengersArrayList.size());
-		System.out.println();
+        while (scan.hasNext()) {
 
-		System.out.println("All avengers in the order they appeared in the input stream:");
-		for (Avenger a : avengersArrayList) {
-			System.out.println(a.toString());
-		}
-		
-		// Todo: Print the list of avengers in the order they appeared in the input
-		// Make sure you follow the formatting example in the sample output
-		System.out.println();
-		
-		System.out.println("Top " + topN + " most popular avengers:");
-		// Todo: Print the most popular avengers, see the instructions for tie breaking
-		// Make sure you follow the formatting example in the sample output
-		System.out.println();
+            String word = scan.next().toLowerCase().replace("[^a-a]", "");
+            if (!word.isEmpty()) {
+                totalwordcount++;
+                for (String[] avenger : avengerRoster) {
+                    if (word.equals(avenger[0]) || word.equals(avenger[1])) {
+                        if (isAlreadyPresent(word)) {
+                            Avenger avenger1 = null;
+                            if ((avenger1 = getAvenger(word)) != null) {
+                                avenger1.incrementFrequency();
+                            }
+                        } else {
+                            avengersArrayList.add(new Avenger(avenger[0], avenger[1]));
+                            avengersArrayList.get(avengersArrayList.size() - 1).incrementFrequency();
+                        }
+                    }
+                }
 
-		System.out.println("Top " + topN + " least popular avengers:");
-		// Todo: Print the least popular avengers, see the instructions for tie breaking
-		// Make sure you follow the formatting example in the sample output		
-		System.out.println();
+            }
 
-		System.out.println("All mentioned avengers in alphabetical order:");
-		// Todo: Print the list of avengers in alphabetical order
-		// Make sure you follow the formatting example in the sample output
-		System.out.println();
-	}
+        }
+    }
+
+    private boolean isAlreadyPresent(String value) {
+        for (Avenger avenger : avengersArrayList) {
+            if (avenger.getHeroName().equals(value) || avenger.getHeroAlias().equals(value)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Avenger getAvenger(String value) {
+        for (Avenger avenger : avengersArrayList) {
+            if (avenger.getHeroName().equals(value) || avenger.getHeroAlias().equals(value)) {
+                return avenger;
+            }
+        }
+        return null;
+    }
+
+    private void printResults() {
+        System.out.println("Total number of words: " + totalwordcount);
+        // Todo: Print the list of avengers in the order they appeared in the input
+        // Make sure you follow the formatting example in the sample output
+        System.out.println("Number of Avengers Mentioned: " + avengersArrayList.size());
+        System.out.println();
+
+        System.out.println("All avengers in the order they appeared in the input stream:");
+        for (Avenger avenger : avengersArrayList) {
+            System.out.println(avenger.toString());
+        }
+        System.out.println();
+
+        System.out.println("Top " + topN + " most popular avengers:");
+        // Todo: Print the most popular avengers, see the instructions for tie breaking
+        // Make sure you follow the formatting example in the sample output
+
+//        ArrayList<Avenger> firstFour = getFirstFour();
+        Collections.sort(avengersArrayList, new AvengerComparator());
+        for(int i = 0; i < avengersArrayList.size(); i++)
+            if(i < 4){
+                System.out.println(avengersArrayList.get(i));
+            }
+        System.out.println();
+
+        System.out.println("Top " + topN + " least popular avengers:");
+        // Todo: Print the least popular avengers, see the instructions for tie breaking
+        // Make sure you follow the formatting example in the sample output	
+        Collections.sort(avengersArrayList, new AvengerComparatorTwo());
+        for(int i = 0; i < avengersArrayList.size(); i++)
+            if(i < 4){
+                System.out.println(avengersArrayList.get(i));
+            }
+        
+        System.out.println();
+
+        System.out.println("All mentioned avengers in alphabetical order:");
+        
+        // Todo: Print the list of avengers in alphabetical order
+        // Make sure you follow the formatting example in the sample output
+        Collections.sort(avengersArrayList);
+        for (Avenger avenger : avengersArrayList) {
+            System.out.println(avenger.toString());
+        }
+        System.out.println();
+    }
+
+    private ArrayList<Avenger> getFirstFour() {
+        Collections.sort(avengersArrayList, new AvengerComparator());
+        ArrayList<Avenger> firstFour = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            if (i < avengersArrayList.size()) {
+                firstFour.add(avengersArrayList.get(i));
+            }
+        }
+        return firstFour;
+    }
+
+    private ArrayList<Avenger> getLeastFour() {
+        Collections.sort(avengersArrayList, new AvengerComparator());
+        ArrayList<Avenger> firstFour = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            if (i < avengersArrayList.size()) {
+                firstFour.add(avengersArrayList.get(i));
+            }
+        }
+        return firstFour;
+    }
 }
