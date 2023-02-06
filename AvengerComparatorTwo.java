a0b98ad0bf1645d612ab10be4404ff156feb31f2
arhamh7 <90596316+arhamh7@users.noreply.github.com>
2023-02-06 16:20:29 -0700

    Create AvengerComparatorTwo.java

---
+++ AvengerComparatorTwo.java
@@ -0,0 +1,19 @@
+import java.util.Comparator;
+
+
+public class AvengerComparatorTwo implements Comparator<Avenger>{
+
+    @Override
+    public int compare(Avenger o1, Avenger o2) {
+         if(o1.getFrequency() > o2.getFrequency())
+            return 1;
+        else if(o1.getFrequency() < o2.getFrequency())
+            return -1;
+        int result = o1.getHeroAlias().length() - o2.getHeroAlias().length();
+        if(result == 0){
+            return o1.getHeroAlias().compareTo(o2.getHeroAlias());
+        }
+        return result;
+    }
+    
+}
