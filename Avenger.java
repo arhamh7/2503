5238eda5b69834e916bc6913064de22b382ab982
arhamh7 <90596316+arhamh7@users.noreply.github.com>
2023-02-06 16:19:39 -0700

    Update Avenger.java

--- Avenger.java
+++ Avenger.java
@@ -1,36 +1,51 @@
+public class Avenger implements Comparable<Avenger>{
+    
+    private String heroName;
+    private String heroAlias;
+    private int frequency;
 
-public class Avenger implements Comparable <Avenger>{
-
-	// TODO: Implement the Avenger Class
-	
-	private String heroAlias; 
-	private String heroName;
-	private int frequency; 
-	
-	public Avenger(String heroName, String heroAlias) {
+    public Avenger(String heroName, String heroAlias) {
         this.heroName = heroName;
         this.heroAlias = heroAlias;
-        this.frequency = 1;
     }
 
-    public void incrementFrequency() {
+    public String getHeroName() {
+        return heroName;
+    }
+
+    public void setHeroName(String heroName) {
+        this.heroName = heroName;
+    }
+
+    public String getHeroAlias() {
+        return heroAlias;
+    }
+
+    public void setHeroAlias(String heroAlias) {
+        this.heroAlias = heroAlias;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
+    }
+    
+    public void incrementFrequency(){
         this.frequency++;
     }
 
+    @Override
+    public int compareTo(Avenger avenger) {
+        return this.getHeroName().compareTo(avenger.getHeroName());
+    }
     
-	
-	@Override
-	public int compareTo(Avenger o) {
-		return this.heroAlias.compareTo(o.heroName);
-	}
-	
-	
-	
-	@Override
+    @Override
 	public String toString() { 
 		String format = heroName + " aka " + heroAlias
                 + " mentioned " + frequency + " time(s)";
 		return format;
 	}
-
 }
